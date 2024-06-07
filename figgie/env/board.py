02d71a091b93540f71752e54efda53a38553aa4c
KaiyuanG <kaiyuang@Princeton.edu>
2024-06-07 16:41:55 -0400

    Version with Class OrderBook

--- figgie/env/board.py
+++ figgie/env/board.py
@@ -9,6 +9,7 @@ class Board:
         self.cards = self.initialize_cards()
         self.initialize_players(num_players, player_ids)
         self.hands = self.deal_cards()
+        self.funds = {player : 200 for player in self.players}
         print(self.hands)
 
     def initialize_cards(self):
@@ -18,8 +19,7 @@ class Board:
         
         twelve_card_suit = suits.pop(0)
         self.goal_suit = suit_pairs[twelve_card_suit]
-        
-        random.shuffle(suits)   
+
         suit_counts = {twelve_card_suit: 12}
         for suit, count in zip(suits, counts[1:]):
             suit_counts[suit] = count
@@ -51,10 +51,51 @@ class Board:
         winners = [player for player, count in goal_suit_counts.items() if count == max_goal_cards]
         print(f"Winners: {winners} with {max_goal_cards} goal suit cards")
 
-        payouts = {player: 10 * goal_suit_counts[player] for player in self.hands}
-
+        score = {player: 10 * goal_suit_counts[player] + self.funds[player] for player in self.hands}
         pot_share = self.pot // len(winners)
         for player in self.winners:
-            payouts[player] += pot_share
+            score[player] += pot_share
             
-        return winners, max_goal_cards, payouts
+        return winners, max_goal_cards, score
+
+class OrderBook:
+    def __init__(self, board):
+        self.board = board
+        suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+        self.Bid = {suit: None for suit in suits}
+        self.Ask = {suit: None for suit in suits}
+        self.LastPrice = {suit: None for suit in suits}
+
+    def place_bid(self, player, suit, price):
+        if price < self.board.funds[player]:
+            current_Ask = self.Ask[suit]
+            if price >= current_Ask['price']:
+                self.make_trade(suit,current_Ask['price'],player,current_Ask['player']) #buyer, seller
+            current_Bid = self.Bid[suit]
+            if current_Bid is None or price > current_Bid['price']:
+                self.Bid[suit] = {'player': player, 'price': price}
+        else:
+            print(f"Failed to place bid: {player} lacks sufficient funds.")
+
+    def place_ask(self, player, suit, price):
+        if self.board.hands[player][suit] >= 1: 
+            current_Bid = self.Bid[suit]
+            if price <= current_Bid['price']:
+                self.make_trade(suit,current_Bid['price'],current_Bid['player'],player)
+            current_Ask = self.Ask[suit]
+            if current_Ask is None or price < current_Ask['price']:
+                self.Ask[suit] = {'player': player, 'price': price}
+        else:
+            print(f"Failed to place bid: {player} lacks sufficient cards.")
+
+    def make_trade(self, suit, price, buyer, seller):
+        print(f"Trade executed between {buyer} and {seller} for {suit} at {price}")
+        self.LastPrice[suit] = price
+        self.board.funds[buyer] -= price
+        self.board.funds[seller] += price
+        self.board.hands[buyer][suit] += 1
+        self.board.hands[seller][suit] -= 1
+
+        suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+        self.Bid = {suit: None for suit in suits}
+        self.Ask = {suit: None for suit in suits}
